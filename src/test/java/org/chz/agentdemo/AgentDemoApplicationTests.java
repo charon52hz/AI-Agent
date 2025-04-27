066ba939f4baf21158e20d77136bdb8daa9c7f02
charon52hz <charon52hz@gmail.com>
2025-04-27 12:21:40 +0800

    init

---
+++ src/test/java/org/chz/agentdemo/AgentDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.chz.agentdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgentDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
